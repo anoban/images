72d9efb82832e410fd0ef39e7fe8155022c45a6e
anoban <2016sb117@univ.jfn.ac.lk>
2023-10-27 17:16:40 +0530

    modified:   bmp.cpp 	modified:   bmp.hpp 	modified:   bmpex.cpp

--- bmp.cpp
+++ bmp.cpp
@@ -1,1 +1,63 @@
 #include "bmp.hpp"
+
+std::vector<uint8_t> bmp::open(_In_ const std::wstring& path, _Out_ uint64_t* const nread_bytes) {
+    *nread_bytes                = 0;
+    HANDLE               handle = CreateFileW(path.c_str(), GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_READONLY, nullptr);
+    std::vector<uint8_t> buff {};
+
+    if (handle == INVALID_HANDLE_VALUE)
+        throw std::runtime_error(std::format("Error {:4d} in CreateFileW [{}{:4d}]\n", GetLastError(), __FILE__, __LINE__));
+
+    LARGE_INTEGER size;
+    if (!GetFileSizeEx(handle, &size)) {
+        CloseHandle(handle);
+        throw std::runtime_error(std::format("Error {:4d} in GetFileSizeEx [{}{:4d}]\n", GetLastError(), __FILE__, __LINE__));
+    }
+
+    buff.resize(size.QuadPart);
+    // allocation errors will be automatically handled by the C++ runtime.
+
+    if (!ReadFile(handle, buff.data(), size.QuadPart, reinterpret_cast<LPDWORD>(nread_bytes), nullptr)) {
+        CloseHandle(handle);
+        throw std::runtime_error(std::format("Error {:4d} in ReadFile [{}{:4d}]\n", GetLastError(), __FILE__, __LINE__));
+    }
+
+    return buff;
+}
+
+bmp::BITMAPFILEHEADER bmp::bmp::parse_fileheader(_In_ const std::vector<uint8_t>& imstream) {
+    static_assert(sizeof(BITMAPFILEHEADER) == 14LLU, "Error: BITMAPFILEHEADER must be 14 bytes in size");
+    assert(imstream.size() >= sizeof(BITMAPFILEHEADER));
+
+    BITMAPFILEHEADER header {};
+    const auto       tmp {
+        std::array<uint8_t, 2> {imstream.at(0), imstream.at(1)}
+    };
+    if (tmp != soi) throw std::runtime_error("Error in bmp::bmp::parse_fileheader, file isn't a Windows BMP file\n");
+    header.FSIZE          = *reinterpret_cast<const uint32_t*>(imstream.data() + 2);
+    header.PIXELDATASTART = *reinterpret_cast<const uint32_t*>(imstream.data() + 10);
+
+    return header;
+}
+
+bmp::BITMAPINFOHEADER bmp::bmp::parse_infoheader(_In_ const std::vector<uint8_t>& imstream) {
+    static_assert(sizeof(BITMAPINFOHEADER) == 40LLU, "Error: BITMAPINFOHEADER must be 40 bytes in size");
+    assert(imstream.size() >= (sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER)));
+
+    BITMAPINFOHEADER header = { 0, 0, 0, 0, 0, COMPRESSIONKIND::UNKNOWN, 0, 0, 0, 0, 0 };
+    if (*reinterpret_cast<const uint32_t*>(imstream.data() + 14U) > 40)
+        throw std::runtime_error("Error: Cannot parse BITMAPINFOHEADERs larger than 40 bytes");
+
+    header.HEADERSIZE    = *reinterpret_cast<const uint32_t*>(imstream.data() + 14);
+    header.WIDTH         = *reinterpret_cast<const uint32_t*>(imstream.data() + 18);
+    header.HEIGHT        = *reinterpret_cast<const uint32_t*>(imstream.data() + 22);
+    header.NPLANES       = *reinterpret_cast<const uint16_t*>(imstream.data() + 26);
+    header.NBITSPERPIXEL = *reinterpret_cast<const uint16_t*>(imstream.data() + 28);
+    header.CMPTYPE       = get_bmp_compression_kind(*((uint32_t*) (imstream.data() + 30U)));
+    header.IMAGESIZE     = *reinterpret_cast<const uint32_t*>(imstream.data() + 34);
+    header.RESPPMX       = *reinterpret_cast<const uint32_t*>(imstream.data() + 38);
+    header.RESPPMY       = *reinterpret_cast<const uint32_t*>(imstream.data() + 42);
+    header.NCMAPENTRIES  = *reinterpret_cast<const uint32_t*>(imstream.data() + 46);
+    header.NIMPCOLORS    = *reinterpret_cast<const uint32_t*>(imstream.data() + 50);
+    return header;
+}
--- bmpex.cpp
+++ bmpex.cpp
@@ -1,47 +1,10 @@
-int wmain(_In_ int argc, _In_ wchar_t* argv[]) {
-    if (argc < 2) {
-        fputs("Error: no files passed, bmp.exe takes/needs one argument: [file name]", stderr);
-        exit(1);
+#include "bmp.hpp"
+
+int wmain(_In_opt_ int32_t argc, _In_opt_count_(argc) wchar_t* argv[]) {
+    for (size_t i = 1; i < argc; ++i) {
+        size_t nbytes {};
+        auto   buffer { bmp::open(argv[i], &nbytes) };
+        ::wprintf_s(L"%s is %zu bytes\n", argv[i], nbytes);
     }
-
-    BMP image  = new_bmp(argv[1]);
-
-    BMP bw_ave = to_blacknwhite(&image, AVERAGE, false);
-    BMP bw_wav = to_blacknwhite(&image, WEIGHTED_AVERAGE, false);
-    BMP bw_lum = to_blacknwhite(&image, LUMINOSITY, false);
-    BMP bw_bin = to_blacknwhite(&image, BINARY, false);
-
-    serialize_bmp(&bw_ave, L"./sydney_bw_ave.bmp");
-    serialize_bmp(&bw_wav, L"./sydney_bw_wav.bmp");
-    serialize_bmp(&bw_lum, L"./sydney_bw_lum.bmp");
-    serialize_bmp(&bw_bin, L"./sydney_bw_bin.bmp");
-
-    close_bmp(bw_ave);
-    close_bmp(bw_wav);
-    close_bmp(bw_lum);
-    close_bmp(bw_bin);
-
-    BMP nored   = remove_color(&image, REMRED, false);
-    BMP nogreen = remove_color(&image, REMGREEN, false);
-    BMP noblue  = remove_color(&image, REMBLUE, false);
-    BMP norg    = remove_color(&image, REMRG, false);
-    BMP norb    = remove_color(&image, REMRB, false);
-    BMP nogb    = remove_color(&image, REMGB, false);
-
-    serialize_bmp(&nored, L"./sydney_GB.bmp");
-    serialize_bmp(&noblue, L"./sydney_RG.bmp");
-    serialize_bmp(&nogreen, L"./sydney_RB.bmp");
-    serialize_bmp(&nogb, L"./sydney_R.bmp");
-    serialize_bmp(&norb, L"./sydney_G.bmp");
-    serialize_bmp(&norg, L"./sydney_B.bmp");
-
-    close_bmp(image);
-    close_bmp(nored);
-    close_bmp(noblue);
-    close_bmp(nogreen);
-    close_bmp(nogb);
-    close_bmp(norb);
-    close_bmp(norg);
-
     return EXIT_SUCCESS;
 }
