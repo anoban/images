732e6c5432cc8f54698d626662995909db474064
anoban <2016sb117@univ.jfn.ac.lk>
2024-03-31 07:33:05 +0530

    new file:   ico/.vscode/launch.json 	new file:   ico/.vscode/settings.json 	new file:   ico/.vscode/tasks.json 	modified:   ico/ico.hpp 	new file:   ico/io.cpp 	deleted:    ico/io.hpp 	new file:   images/ico/crown-bmp.ico 	new file:   images/ico/folder-png.ico 	new file:   images/ico/python-png.ico 	new file:   images/ico/stars-bmp.ico

--- ico/ico.hpp
+++ ico/ico.hpp
@@ -2,8 +2,23 @@
 #ifndef __ICO_HPP__
     #define __ICO_HPP__
 
+    #include <optional>
+    #include <vector>
+
 namespace ico {
-    class ico { }; // class ico
+
+    namespace io {
+        std::optional<std::vector<uint8_t>> Open(_In_ const wchar_t* const filename) noexcept;
+        bool                                Serialize(_In_ const wchar_t* const filename, _In_ const std::vector<uint8_t>& buffer) noexcept;
+    } // namespace io
+
+    class ico {
+        public:
+            ico() = delete; // no default ctors
+            inline ico(_In_ const wchar_t* const filename) noexcept;
+
+        private:
+    }; // class ico
 } // namespace ico
 
 #endif // !__ICO_HPP__
