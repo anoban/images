7773407e11c26b52eb529ca91d874952edb482b9
anoban <2016sb117@univ.jfn.ac.lk>
2024-10-28 00:18:35 +0530

    renamed:    include/png.h -> include/png 	deleted:    include/png.c 	new file:   tests/png.cpp 	deleted:    tests/test.cpp

--- include/png.c
+++
@@ -1,5 +0,0 @@
-#include <png.h>
-
-bool __stdcall IsValidPngChunk(_In_ const chunk_t* const restrict chunk) {
-    return isascii(chunk->pcType[0]) && isascii(chunk->pcType[1]) && isascii(chunk->pcType[2]) && isascii(chunk->pcType[3]);
-}
