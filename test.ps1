c9d082785bdb8c00cb62d3c99602e97e18b9683e
anoban <2016sb117@univ.jfn.ac.lk>
2024-10-31 08:42:16 +0530

    deleted:    googletest/src/gtest_main.cc 	modified:   include/bitmap 	modified:   include/helpers 	new file:   test.ps1 	modified:   tests/bitmap.cpp

---
+++ test.ps1
@@ -0,0 +1,62 @@
+$cfiles = [System.Collections.ArrayList]::new()
+$unrecognized = [System.Collections.ArrayList]::new()
+
+
+foreach ($arg in $args) {
+    if ($arg -clike "*.cpp") {
+        [void]$cfiles.Add($arg.ToString().Replace(".\", ""))
+    }
+    else {
+        [void]$unrecognized.Add($arg)
+    }
+}
+
+if ($unrecognized.Count -ne 0) {
+    Write-Error "Incompatible files passed for compilation: ${unrecognized}"
+    Exit 1
+}
+
+$cflags = @(
+    "./tests/main.cpp", # contains the main() function
+    "./googletest/src/gtest-all.cc", # convenience source file that bundles all the necessary gtest sources
+    "/arch:AVX512",
+    "/diagnostics:caret",
+    "/DNDEBUG",
+    "/D_NDEBUG",
+    "/EHac",
+    "/F0x10485100",
+    "/favor:INTEL64",
+    "/Fe:test.exe",
+    "/fp:strict",
+    "/fpcvt:IA",
+    "/GL",
+    "/Gw",
+    "/I./include/",
+    "/I./googletest/",
+    "/I./googletest/include/",
+    "/jumptablerdata",
+    "/MP",
+    "/MT", # statically link the multithreaded version of Windows libs
+    "/O2",
+    "/Ob3",
+    "/Oi",
+    "/Ot",
+    "/Qpar",
+    "/std:c++14",
+    "/TP",
+    "/Wall",
+    "/wd4514",      # removed unreferenced inline function
+    "/wd4625",      # gtest
+    "/wd4626",      # gtest
+    "/wd5026",      # gtest
+    "/wd5027",      # gtest
+    "/wd4710",      # not inlined
+    "/wd4711",      # selected for inline expansion
+    "/wd4820",      # struct padding
+    "/Zc:__cplusplus",
+    "/Zc:preprocessor",
+    "/link /DEBUG:NONE"
+)
+
+Write-Host "cl.exe ${cfiles} ${cflags}" -ForegroundColor Cyan
+cl.exe $cfiles $cflags
--- tests/bitmap.cpp
+++ tests/bitmap.cpp
@@ -1,3 +1,9 @@
 #include <bitmap>
 
 #include <gtest/gtest.h>
+
+TEST(BITMAP, FROM_FILE) {
+    bitmap elsa { LR"(./castle.bmp)" };
+    elsa.to_blacknwhite(BW_TRANSFORMATION::AVERAGE);
+    elsa.to_file(LR"(./bw.bmp)");
+}
