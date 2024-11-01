b5dec93ced78cba09df15f3cc47307531ddf5074
anoban <2016sb117@univ.jfn.ac.lk>
2024-11-01 07:03:28 +0530

    modified:   include/bitmap 	modified:   tests/bitmap.cpp

--- tests/bitmap.cpp
+++ tests/bitmap.cpp
@@ -2,6 +2,31 @@
 
 #include <gtest/gtest.h>
 
+inline namespace __colour_removers {
+
+    TEST(COLOUR_REMOVER, REMOVE) {
+        RGBQUAD pixel { 255, 255, 255, 255 };
+
+        colour_removers::remove<RGB_TAG::BLUE> {}(pixel);
+        EXPECT_EQ(pixel, (RGBQUAD { 0, 255, 255, 255 })); // the surrounding parenthesis is important
+        colour_removers::remove<RGB_TAG::GREEN> {}(pixel);
+        EXPECT_EQ(pixel, (RGBQUAD { 0, 0, 255, 255 }));
+        colour_removers::remove<RGB_TAG::RED> {}(pixel);
+        EXPECT_EQ(pixel, (RGBQUAD { 0, 0, 0, 255 }));
+
+        pixel = { 255, 255, 255, 255 };
+        colour_removers::remove<RGB_TAG::GREENBLUE> {}(pixel);
+        EXPECT_EQ(pixel, (RGBQUAD { 0, 0, 255, 255 }));
+        pixel = { 255, 255, 255, 255 };
+        colour_removers::remove<RGB_TAG::REDBLUE> {}(pixel);
+        EXPECT_EQ(pixel, (RGBQUAD { 0, 255, 0, 255 }));
+        pixel = { 255, 255, 255, 255 };
+        colour_removers::remove<RGB_TAG::REDGREEN> {}(pixel);
+        EXPECT_EQ(pixel, (RGBQUAD { 255, 0, 0, 255 }));
+    }
+
+} // namespace __colour_removers
+
 TEST(BITMAP, FROM_FILE) {
     bitmap image { LR"(./black.bmp)" };
     image.make_blacknwhite(BW_TRANSFORMATION::AVERAGE).to_file(LR"(./bw_average.bmp)");
