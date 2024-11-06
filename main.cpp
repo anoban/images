0eab5cb1c45922e665630689d339faa50d5c7cf2
anoban <2016sb117@univ.jfn.ac.lk>
2024-11-06 07:26:09 +0530

    new file:   images/ico/intel.ico 	modified:   include/bitmap 	new file:   include/canvas 	modified:   include/ico 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <ico>
 
-int wmain() {
-    const icon_directory image { LR"(.\images\ico\crown-bmp.ico)" };
+int wmain(_In_ const int argc, _In_ const wchar_t** const argv) {
+    if (argc == 1) return EXIT_FAILURE;
+
+    for (int i = 1; i < argc; ++i) {
+        const icon_directory image { argv[i] };
+        std::wcout << argv[i] << "  " << image.image_count() << L'\n';
+    }
+
     return EXIT_SUCCESS;
 }
