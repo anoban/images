e68ac3fd7e8ff2b1aced7c12883f5bdc9befaeb9
anoban <2016sb117@univ.jfn.ac.lk>
2024-11-11 06:29:10 +0530

    modified:   include/bitmap 	modified:   include/canvas 	modified:   include/helpers 	modified:   include/png 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
+#include <canvas>
 #include <ico>
 
 int wmain(_In_ const int argc, _In_ const wchar_t** const argv) {
-    if (argc == 1) return EXIT_FAILURE;
+    // if (argc == 1) return EXIT_FAILURE;
 
-    for (int i = 1; i < argc; ++i) {
-        const icon_directory image { argv[i] };
-        std::wcout << argv[i] << "  " << image.image_count() << L'\n';
-    }
+    bitmap image { LR"(./black.bmp)" };
+    canvas board { std::move(image) };
+
+    board.fill({ 0x00, 0x0A, 0x0A, 0xFF });
+    board.to_file(LR"(canvas.bmp)");
 
     return EXIT_SUCCESS;
 }
