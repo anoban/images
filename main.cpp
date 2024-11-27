c30f03e66aef3faa8d78d287821406c2078acc79
anoban <2016sb117@univ.jfn.ac.lk>
2024-11-27 06:18:50 +0530

    modified:   include/fractal 	modified:   include/helpers 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,27 +3,11 @@
 #include <fractal>
 
 int wmain(_In_opt_ int argc, _In_opt_count_c_(argc) const wchar_t* const argv[]) {
-    fractal image { 1500, 1000 };
+    fractal image { 1000, 1000 };
     image.waves();
     image.to_file(LR"(.\waves.bmp)");
     image.mandelbrot(colourmaps::COPPER);
     image.to_file(LR"(.\mandelbrot.bmp)");
-
-    // board.to_blacknwhite<rgb::BW_TRANSFORMATION::AVERAGE>().to_file(LR"(average.bmp)");
-    // board.to_blacknwhite<rgb::BW_TRANSFORMATION::WEIGHTED_AVERAGE>().to_file(LR"(weighted_average.bmp)");
-    // board.to_blacknwhite<rgb::BW_TRANSFORMATION::LUMINOSITY>().to_file(LR"(luminosity.bmp)");
-    // board.to_blacknwhite<rgb::BW_TRANSFORMATION::BINARY>().to_file(LR"(binary.bmp)");
-
-    // board.remove_colour<rgb::RGB_TAG::RED>().to_file(LR"(bluegreen.bmp)");
-    // board.remove_colour<rgb::RGB_TAG::GREEN>().to_file(LR"(redblue.bmp)");
-    // board.remove_colour<rgb::RGB_TAG::BLUE>().to_file(LR"(redgreen.bmp)");
-    // board.remove_colour<rgb::RGB_TAG::GREENBLUE>().to_file(LR"(red.bmp)");
-    // board.remove_colour<rgb::RGB_TAG::REDBLUE>().to_file(LR"(green.bmp)");
-    // board.remove_colour<rgb::RGB_TAG::REDGREEN>().to_file(LR"(blue.bmp)");
-
-    // canvas fractal(5000, 5000);
-    // fractal.mandelbrot(colourmaps::COPPER);
-    // fractal.to_file(LR"(.\mandelbrot.bmp)");
 
     return EXIT_SUCCESS;
 }
