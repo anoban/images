911e9cd04793ff6b0f97bae02caa4e1217e6548e
anoban <2016sb117@univ.jfn.ac.lk>
2024-11-27 06:34:53 +0530

    new file:   debug.ps1 	modified:   include/fractal 	modified:   include/helpers 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <fractal>
 
 int wmain(_In_opt_ int argc, _In_opt_count_c_(argc) const wchar_t* const argv[]) {
-    fractal image { 1000, 1000 };
+    fractal image { 100, 100 };
     image.waves();
     image.to_file(LR"(.\waves.bmp)");
     image.mandelbrot(colourmaps::COPPER);
