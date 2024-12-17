32441cdb51a2da24af898a4e36e75634bf4b08c5
anoban <2016sb117@univ.jfn.ac.lk>
2024-12-17 06:09:15 +0530

    new file:   include/_png.hpp 	new file:   include/png

---
+++ include/_png.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#define __INTERNAL
+#if !defined(__IMAGEIO) && !defined(__INTERNAL) && !defined(__TEST__)
+    #error DO NOT DIRECTLY INCLUDE HEADERS PREFIXED WITH AN UNDERSCORE IN SOURCE FILES, USE THE UNPREFIXED VARIANTS WITHOUT THE .HPP EXTENSION.
+#endif
+
+class png final {
+    public:
+        static constexpr unsigned long long PNG_CHUNK_NAME_LENGTH { 4 };
+
+        class png_chunk final {
+                unsigned       length;
+                char           name[PNG_CHUNK_NAME_LENGTH];
+                unsigned char* data;
+                unsigned       crc;
+        };
+
+    private:
+
+    public:
+};
+
+#undef __INTERNAL
