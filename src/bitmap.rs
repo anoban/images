d83350b20528f656308836220bdd830384a8cc27
anoban <2016sb117@univ.jfn.ac.lk>
2025-07-26 12:12:27 +1000

    modified:   src/bitmap.rs

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -10,5 +10,34 @@ pub mod bitmap {
 
      */
 
-    pub struct Bitmap {}
+    #[repr(C)]
+    struct RgbQuad {
+        /*
+        typedef struct tagRGBQUAD {
+          BYTE rgbBlue;
+          BYTE rgbGreen;
+          BYTE rgbRed;
+          BYTE rgbReserved;
+        } RGBQUAD;
+        */
+        blue: u8,
+        green: u8,
+        red: u8,
+        reserved: u8,
+    }
+
+    #[repr(C)]
+    struct BitmapFileHeader {
+        /*
+        typedef struct tagBITMAPFILEHEADER {
+          WORD  bfType;
+          DWORD bfSize;
+          WORD  bfReserved1;
+          WORD  bfReserved2;
+          DWORD bfOffBits;
+        } BITMAPFILEHEADER, *LPBITMAPFILEHEADER, *PBITMAPFILEHEADER;
+        */
+    }
+
+    pub struct BitMap {}
 }
