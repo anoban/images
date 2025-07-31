1acd5fa79512398220dcd80ea61f484e321c2014
anoban <2016sb117@univ.jfn.ac.lk>
2025-08-01 08:53:40 +1000

    deleted:    arrays.rs 	modified:   include/_cmaps.h 	new file:   rustfmt.toml 	modified:   src/colormaps.rs

--- include/_cmaps.h
+++ include/_cmaps.h
@@ -10,11 +10,7 @@
 #include <windef.h>
 #include <wingdi.h>
 
-// colourmaps courtesy of https://github.com/ArashPartow/bitmap/blob/master/bitmap_image.hpp
-// his colourmaps use struct rgb_t { unsigned char red; unsigned char green; unsigned char blue; }; as the pixel type (RGB sequence)
-// hence they have been refactored to comply with GDI's RGBQUAD struct, which sequences colours in BGR order
 
-// contains nine colourmaps named YARG, VGA, PRISM, JET, HSV, HOT, GREY, COPPER and AUTUMN
 
 static constexpr unsigned CMAPSIZE  = 1000;
 
