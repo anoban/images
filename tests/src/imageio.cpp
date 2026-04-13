1a7ed897cbe54d8c0f33e8c584219993dbda7d56
anoban <2016sb117@univ.jfn.ac.lk>
2026-04-13 18:09:52 +1000

    deleted:    tests/media/romeoandjuliet.txt 	deleted:    tests/media/wutheringheights.txt 	modified:   tests/src/imageio.cpp

--- tests/src/imageio.cpp
+++ tests/src/imageio.cpp
@@ -1,4 +1,33 @@
 // clang-format off
 #include <_utilities.hpp>
+#include <_imageio.hpp>
 #include <gtest/gtest.h>
 // clang-format on
+
+TEST(IO, imopen) {
+    long readbytes {};
+
+    const unsigned char* buffer = io::imopen("R(./media/nonexistent.bin)", readbytes);
+    ASSERT_EQ(readbytes, 0);
+    ASSERT_EQ(buffer, nullptr);
+
+    buffer = io::imopen("R(./media/janeeyre.txt)", readbytes);
+    ASSERT_EQ(readbytes, 0);
+    ASSERT_EQ(buffer, nullptr);
+
+    buffer = io::imopen("R(./media/warandpeace.txt)", readbytes);
+    ASSERT_EQ(readbytes, 0);
+    ASSERT_EQ(buffer, nullptr);
+
+    buffer = io::imopen("R(./media/image.jpg)", readbytes);
+    ASSERT_EQ(readbytes, 0);
+    ASSERT_EQ(buffer, nullptr);
+
+    buffer = io::imopen("R(./media/python313.dll)", readbytes);
+    ASSERT_EQ(readbytes, 0);
+    ASSERT_EQ(buffer, nullptr);
+}
+
+TEST(IO, imwrite) {
+    //
+}
